15aef2a597e9fd894f28a9c4d94b2ec13bd6157c
Roy van Elk <royvelk@gmail.com>
2017-01-18 09:45:05 +0100

    WIP

---
+++ models/metadata.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Metadata extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function open()
+    {
+
+    }
+
+    public function write()
+    {
+
+    }
+
+    public function exists()
+    {
+
+    }
+
+    public function close()
+    {
+
+    }
+}
+
