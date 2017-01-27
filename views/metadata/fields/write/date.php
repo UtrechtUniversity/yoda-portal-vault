0d9f4311672b3217fb2483b0ae50c659fecd2cb9
Roy van Elk <royvelk@gmail.com>
2017-01-27 16:47:02 +0100

    Duplicate field elements

--- views/metadata/fields/write/date.php
+++ views/metadata/fields/write/date.php
@@ -3,7 +3,7 @@
         <?php echo $e->label; ?>
         <i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" title="<?php echo $e->helpText; ?>"></i>
         <?php if ($e->mandatory) { ?>
-            <i class="fa fa-lock safe" aria-hidden="true" data-toggle="tooltip" title="Required for the safe"></i>
+            <i class="fa fa-lock safe" aria-hidden="true" data-toggle="tooltip" title="Required for the vault"></i>
         <?php } ?>
     </label>
     <div class="col-sm-6">
@@ -12,7 +12,7 @@
             <div class="input-group">
                 <input type="text" class="form-control datepicker" name="<?php echo $e->key; ?>[]" readonly="true">
                 <span class="input-group-btn">
-                    <button class="btn btn-default extend-field" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                    <button class="btn btn-default duplicate-field" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
                 </span>
             </div>
         <?php } else { ?>
--- views/metadata/fields/write/select.php
+++ views/metadata/fields/write/select.php
@@ -3,7 +3,7 @@
         <?php echo $e->label; ?>
         <i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" title="<?php echo $e->helpText; ?>"></i>
         <?php if ($e->mandatory) { ?>
-        <i class="fa fa-lock safe" aria-hidden="true" data-toggle="tooltip" title="Required for the safe"></i>
+        <i class="fa fa-lock safe" aria-hidden="true" data-toggle="tooltip" title="Required for the vault"></i>
         <?php } ?>
     </label>
     <div class="col-sm-3">
@@ -17,7 +17,7 @@
                     <?php } ?>
                 </select>
                 <span class="input-group-btn">
-                    <button class="btn btn-default extend-field" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                    <button class="btn btn-default duplicate-field" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
                 </span>
             </div>
         <?php } else { ?>
