194b189b435f252b6c7a70ceb92e4415b4795709
Roy van Elk <royvelk@gmail.com>
2017-02-17 17:07:59 +0100

    ui metadata

--- views/metadata/form.php
+++ views/metadata/form.php
@@ -1,10 +1,14 @@
 <?php if (empty($form)) { ?>
 <div class="row">
     <div class="col-md-12">
-        <a class="btn btn-default" href="/research/browse?dir=<?php echo $path; ?>">Back to overview</a>
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Metadata form - <?php echo $path; ?></h3>
+        <div class="panel panel-default">
+            <div class="panel-heading clearfix">
+                <h3 class="panel-title pull-left">
+                    Metadata form - <?php echo $path; ?>
+                </h3>
+                <div class="input-group-sm has-feedback pull-right">
+                    <a class="btn btn-default" href="/research/browse?dir=<?php echo $path; ?>">Close</a>
+                </div>
             </div>
             <div class="panel-body">
                 <p>It is not possible to load this form due to the formatting of .yoda-metadata.xml.<br>
@@ -24,10 +28,14 @@
     <div class="row">
         <div class="col-md-12">
             <?php echo $form->open('research/metadata/store?path=' . $path, 'form-horizontal metadata-form'); ?>
-            <a class="btn btn-default" href="/research/browse?dir=<?php echo $path; ?>">Back to overview</a>
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Metadata form - <?php echo $path; ?></h3>
+            <div class="panel panel-default">
+                <div class="panel-heading clearfix">
+                    <h3 class="panel-title pull-left">
+                        Metadata form - <?php echo $path; ?>
+                    </h3>
+                    <div class="input-group-sm has-feedback pull-right">
+                        <a class="btn btn-default" href="/research/browse?dir=<?php echo $path; ?>">Close</a>
+                    </div>
                 </div>
                 <div class="panel-body">
                     <?php if ($form === false) { ?>
@@ -45,7 +53,7 @@
                         <div class="form-group">
                             <div class="col-sm-12">
                                 <?php if ($form->getPermission() == 'write') { ?>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
                                 <?php } ?>
                                 <?php if ($userType != 'reader' && $metadataExists) { ?>
                                     <button type="button" class="btn btn-danger delete-all-metadata-btn pull-right" data-path="<?php echo $path; ?>">Delete all metadata</button>
@@ -67,7 +75,7 @@
                         <div class="form-group">
                             <div class="col-sm-12">
                                 <?php if ($form->getPermission() == 'write') { ?>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
                                 <?php } ?>
                                 <?php if ($userType != 'reader' && $metadataExists) { ?>
                                     <button type="button" class="btn btn-danger delete-all-metadata-btn pull-right" data-path="<?php echo $path; ?>">Delete all metadata</button>
