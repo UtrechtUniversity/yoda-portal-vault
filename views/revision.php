186404d4a96071e6756b6c01fccda573c17687f6
Harm de Raaff <harm.apple@gmail.com>
2017-05-15 18:09:02 +0200

    Esthetic changes so all dialogs are in line

--- views/revision.php
+++ views/revision.php
@@ -7,36 +7,56 @@
 <div class="modal" id="select-folder">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
-                <span class="modal-title">Select folder to restore revision</span>
-            </div>
 
-            <input type="hidden" id="restoration-objectid" value="">
+            <div class="panel-body">
 
-            <ol class="breadcrumb dlg-breadcrumb">
-                <li class="active">Home</li>
-            </ol>
+                <div class="panel panel-default">
+                    <div class="panel-heading clearfix">
+                        <h3 class="panel-title pull-left">
+                            Select folder to restore revision
+                        </h3>
 
-            <div class="modal-body">
-                <div class="panel panel-default revision-restore-dialog">
-                    <div class="panel-body">
-
-                        <table id="folder-browser" class="table yoda-table table-bordered">
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Modified date</th>
-                            </tr>
-                            </thead>
-                        </table>
+                        <div class="input-group-sm has-feedback pull-right">
+    <!--                        <button class="btn btn-default"  id="btn-cancel-overite-dialog" >Cancel</button>-->
+                            <button class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
                     </div>
+
+                    <input type="hidden" id="restoration-objectid" value="">
+<!--                    <br>-->
+
+<!--                    <div class="modal-body">-->
+                        <div class="panel revision-restore-dialog">
+                            <div class="panel-body">
+                                <ol class="breadcrumb dlg-breadcrumb">
+                                    <li class="active">Home</li>
+                                </ol>
+
+                                <table id="folder-browser" class="table yoda-table table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Modified date</th>
+                                    </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+<!--                    </div>-->
+
+
                 </div>
             </div>
+<!--            <div class="modal-header">-->
+<!--                <span class="modal-title">Select folder to restore revision</span>-->
+<!--            </div>-->
+
+
 
             <div class="modal-footer">
-                <button class="btn button btn-success" id="btn-restore"><i class="fa fa-magic" aria-hidden="true"></i> Restore your file</button>
-                <button class="btn button grey" data-dismiss="modal">Close</button>
+                <button class="btn btn-default" id="btn-restore"><i class="fa fa-magic" aria-hidden="true"></i> Restore</button>
             </div>
+
             <div id="coverAll" class="cover restore-exists hide">
                 <div class="panel panel-default">
                     <div class="panel-heading clearfix">
@@ -45,7 +65,8 @@
                         </h3>
 
                         <div class="input-group-sm has-feedback pull-right">
-                            <button class="btn btn-default"  id="btn-cancel-overite-dialog" >Cancel</button>
+                            <button class="btn btn-default" data-dismiss="modal">Close</button>
+<!--                            <button class="btn btn-default"  id="btn-cancel-overite-dialog" >Cancel</button>-->
                         </div>
                     </div>
                     <div class="panel-body">
@@ -58,19 +79,27 @@
                                 The file <span id="duplicate"></span> The renamed file already you try to add already exists.
                             </div>
 
-                            <form class="form-inline pull-left">
-                                <p>Overwrite this file</p>
-                                <button class="btn btn-danger" id="btn-restore-overwrite">Overwrite</button>
-                            </form>
+                            <div class="panel-body">
+                                <div class="row">
+                                    <form class="form-inline pull-left">
+                                        <p>Overwrite this file</p>
+                                        <button class="btn btn-danger" id="btn-restore-overwrite">Overwrite</button>
+                                    </form>
 
-                            <form class="form-inline pull-right">
-                                <p>Enter new name for the revision you want to restore</p>
-                                <div class="form-group">
-                                    <label for="newFileName">New filename</label>
-                                    <input type="text"  class="form-control" placeholder="Enter new filename" id="newFileName">
+                                    <form class="form-inline pull-right">
+                                        <p>Enter new name for the revision you want to restore</p>
+                                        <div class="form-group">
+                                            <label for="newFileName">New filename</label>
+                                            <input type="text"  class="form-control" placeholder="Enter new filename" id="newFileName">
+                                        </div>
+                                        <button  class="btn btn-primary" id="btn-restore-next-to">Restore with a new filename</button>
+                                    </form>
                                 </div>
-                                <button  class="btn btn-primary" id="btn-restore-next-to">Restore with a new filename</button>
-                            </form>
+                                <div class="row">
+                                    <hr>
+                                    <button class="btn btn-default pull-right"  id="btn-cancel-overite-dialog" >Cancel</button>
+                                </div>
+                            </div>
                         </div>
                         <div class="mode-dlg-locked hide">
                             <div class="alert alert-danger">
@@ -82,7 +111,7 @@
                                 <br>Please select another folder for placement of your revision.
                             </div>
 
-                            <button class="btn btn-default"  id="btn-select-other-folder" >Select other folder...</button>
+                            <button class="btn btn-default pull-right"  id="btn-select-other-folder" >Select other folder...</button>
                         </div>
 
                     </div>
