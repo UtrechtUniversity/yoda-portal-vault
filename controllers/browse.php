c28804320d82010423530e23cb7223bffb8d4c0e
Harm de Raaff <harm.apple@gmail.com>
2017-08-14 11:42:58 +0200

    Action log visible

--- controllers/browse.php
+++ controllers/browse.php
@@ -124,6 +124,36 @@ class Browse extends MY_Controller
         echo json_encode(array('result' => $locks, 'status' => $result['*status'], 'statusInfo' => $result['*statusInfo']));
     }
 
+    public function list_actionLog()
+    {
+        $rodsaccount = $this->rodsuser->getRodsAccount();
+        $pathStart = $this->pathlibrary->getPathStart($this->config);
+        $folderPath = $this->input->get('folder');
+        $fullPath = $pathStart . $folderPath;
+
+        $result = $this->filesystem->listActionLog($rodsaccount, $fullPath);
+
+        $logItems = array();
+        if ($result['*status'] == 'Success') {
+
+            foreach($result['*result'] as $item){
+                $timestamp = gmdate("Y/m/d H:i:s", 1 * $item[0]);
+
+                #user zone handling - users can originate from different zones
+                $parts = explode('#',$item[2]);
+                $userName = $parts[0]; // take developer name without zone initially
+                if (count($parts)==2 &&
+                    $parts[1] != $rodsaccount->zone ) { // ONly present zone when user mentioned in log is not from current zone
+                        $userName = $item[2];
+                }
+                $logItems[] = array($userName, ucwords($item[1]), $timestamp);
+            }
+        }
+
+        echo json_encode(array('result' => $logItems, 'status' => $result['*status'], 'statusInfo' => $result['*statusInfo']));
+    }
+
+
     /**
      * @param int $restrict
      * @param string $interveneOnMetadataKeys
--- views/browse.php
+++ views/browse.php
@@ -42,6 +42,7 @@
             </div>
         </div>
         <ul class="list-group lock-items"></ul>
+        <ul class="list-group actionlog-items"></ul>
     </div>
 
 
