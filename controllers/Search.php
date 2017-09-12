5c0bc9b6585b496921759ca1ad6441178edba54f
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-09-12 10:46:53 +0200

    Fix YDA-877, iRODS special chars in metadata search.

--- controllers/Search.php
+++ controllers/Search.php
@@ -78,7 +78,7 @@ class Search extends MY_Controller
                 0 => 'DATA_NAME',
                 1 => 'COLL_NAME'
             );
-            $result = $this->filesystem->searchByName($rodsaccount, $path, $searchString, "DataObject", $orderColumns[$orderColumn], $orderDir, $length, $start);
+            $result = $this->filesystem->searchByName($rodsaccount, $path, $searchStringEscaped, "DataObject", $orderColumns[$orderColumn], $orderDir, $length, $start);
 
             $status = $result['status'];
             $statusInfo = $result['statusInfo'];
@@ -103,7 +103,7 @@ class Search extends MY_Controller
             $orderColumns = array(
                 0 => 'COLL_NAME'
             );
-            $result = $this->filesystem->searchByName($rodsaccount, $path, $searchString, "Collection", $orderColumns[$orderColumn], $orderDir, $length, $start);
+            $result = $this->filesystem->searchByName($rodsaccount, $path, $searchStringEscaped, "Collection", $orderColumns[$orderColumn], $orderDir, $length, $start);
 
             $status = $result['status'];
             $statusInfo = $result['statusInfo'];
