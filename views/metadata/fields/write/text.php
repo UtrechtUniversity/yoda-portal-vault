3b7fec060b5f645875a01a1ab573d75bc24b3699
Roy van Elk <royvelk@gmail.com>
2017-10-23 14:27:53 +0200

    Metadata form style fixes

--- views/metadata/fields/write/text.php
+++ views/metadata/fields/write/text.php
@@ -1,3 +1,41 @@
+<?php if ($e->compoundFieldCount > 0) { // Compound field structure ?>
+
+    <?php if ($e->compoundFieldPosition == 0) { // First field, add offset. ?>
+        <div class="col-sm-4 col-sm-offset-3 no-padding">
+    <?php } else { ?>
+        <div class="col-sm-4">
+    <?php } ?>
+        <label class="control-label">
+            <?php if ($e->subPropertiesRole=='subPropertyStartStructure'): ?>
+                <i data-structure-id="<?php echo $e->subPropertiesStructID; ?>" class="glyphicon glyphicon-chevron-down subproperties-toggle" data-toggle="tooltip" title="Click to open or close view on subproperties" data-html="true"></i>&nbsp;
+            <?php endif; ?>
+
+            <span data-toggle="tooltip" title="<?php echo $e->helpText; ?>">
+                <?php echo $e->label; ?>
+            </span>
+        </label>
+        <input type="text"
+            <?php if($e->maxLength>0) { echo 'maxlength="' . $e->maxLength .'"'; } ?>
+               class="form-control"
+            <?php if ($e->subPropertiesRole=='subPropertyStartStructure'): ?>
+                data-structure-id="<?php echo $e->subPropertiesStructID; ?>"
+                placeholder="If left empty corresponding subproperties are deleted when saving"
+            <?php endif; ?>
+               name="<?php echo $e->key; ?>"
+               value="<?php echo htmlentities($e->value); ?>">
+    </div>
+
+    <?php if ( $e->compoundMultipleAllowed AND   $e->compoundFieldPosition ==($e->compoundFieldCount-1) ) { // present button only when compound is clonable and at end of element range ?>
+
+        <span class="input-group-btn">
+            <button class="btn btn-default duplicate-field combined-plus"
+                    data-clone="combined"  type="button">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+            </button>
+        </span>
+
+    <?php } ?>
+<?php } else { ?>
 <div class="form-group" xmlns="http://www.w3.org/1999/html">
     <label class="col-sm-2 control-label">
         <?php if ($e->subPropertiesRole=='subPropertyStartStructure'): ?>
@@ -72,3 +110,4 @@
         <?php  get_instance()->load->view('metadata/fields/write/compound-duplicate-button', array('e',$e)); ?>
     </div>
 </div>
+<?php } ?>
