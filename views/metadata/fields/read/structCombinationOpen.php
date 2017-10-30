c6e589ce8b1767b65bab915009d9abf3cb88aa8a
Harm de Raaff <harm.apple@gmail.com>
2017-10-30 09:22:58 +0100

    label in readonly view as well

--- views/metadata/fields/read/structCombinationOpen.php
+++ views/metadata/fields/read/structCombinationOpen.php
@@ -1,8 +1,19 @@
 <div class="combination-start form-group">
-<!--
-<hr>
-COMBINATION  Start tag <?php echo $e->key; ?>
-<br>Is mandatory? <?php echo $e->mandatory ? 'YES' : 'NO' ?>
-<br>Multi? <?php echo $e->multipleAllowed() ? 'YES' : 'NO' ?>
-<hr>
--->
+    <?php // Only toplevel compounds can have titles o subPropertuesBase must be empty
+    if (!$e->subPropertiesBase) { ?>
+
+        <label class="col-sm-2 control-label">
+                <span data-toggle="tooltip" title="<?php echo $e->helpText; ?>">
+                    <?php echo $e->label; ?>
+                </span>
+        </label>
+
+    <?php } ?>
+
+    <!--
+    <hr>
+    COMBINATION  Start tag <?php echo $e->key; ?>
+    <br>Is mandatory? <?php echo $e->mandatory ? 'YES' : 'NO' ?>
+    <br>Multi? <?php echo $e->multipleAllowed() ? 'YES' : 'NO' ?>
+    <hr>
+    -->
