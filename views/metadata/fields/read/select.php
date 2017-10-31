a7eb54b234f5ed6793c56c4f6162c08ea6f5784e
Harm de Raaff <harm.apple@gmail.com>
2017-10-31 13:32:21 +0100

    label added

--- views/metadata/fields/read/select.php
+++ views/metadata/fields/read/select.php
@@ -1,10 +1,17 @@
 <?php if ($e->compoundFieldCount > 0) { // Compound field structure ?>
 
-<?php if ($e->compoundFieldPosition == 0) { // First field, add offset. ?>
-<div class="col-sm-4 col-sm-offset-2 no-padding">
-    <?php } else { ?>
-    <div class="col-sm-4">
+    <?php if ($e->compoundFieldPosition == 0) { // First field, add offset. ?>
+         <?php if (!$e->subPropertiesBase ) { // This is first field of a compound ?>
+             <div class="col-sm-4 col-sm-offset-2 no-padding">
+        <?php }
+            else // This is a start of a compound as a subproperty -> adjust space to accommodate label
+            { ?>
+            <div class="col-sm-4 col-sm-offset-1 no-padding">
         <?php } ?>
+
+    <?php } else { ?>
+        <div class="col-sm-4">
+    <?php } ?>
         <label><?php echo $e->label; ?></label>
         <p class="form-control-static"><?php echo htmlentities($e->value); ?></p>
     </div>
--- views/metadata/fields/read/structCombinationOpen.php
+++ views/metadata/fields/read/structCombinationOpen.php
@@ -8,6 +8,16 @@
                 </span>
         </label>
 
+    <?php } else { // This is a compound witin a subproperty structure -> sm-1 instead of sm-2
+        //  - This only works with select as being a first element as an addition is required for each element!
+        // - at the moment only implemeneted for selects
+        ?>
+        <label class="col-sm-1 control-label">
+                <span data-toggle="tooltip" title="<?php echo $e->helpText; ?>">
+                    <?php echo $e->label; ?>
+                </span>
+        </label>
+
     <?php } ?>
 
     <!--
