3def9d1fc27d6f7244408b1877c2abc1aa9ce09f
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-07-26 12:24:56 +0200

    Move CSRF stuff to seperate file.

--- public/static/js/research.js
+++ public/static/js/research.js
@@ -1,12 +1,3 @@
-$(document).ajaxSend(function(e, request, settings) {
-    // Append a CSRF token to all AJAX POST requests.
-    if (settings.type === 'POST' && settings.data.length) {
-         settings.data
-             += '&' + encodeURIComponent(YodaPortal.csrf.tokenName)
-              + '=' + encodeURIComponent(YodaPortal.csrf.tokenValue);
-    }
-});
-
 $( document ).ready(function() {
     if ($('#file-browser').length) {
         startBrowsing(browseStartDir, browsePageItems);
