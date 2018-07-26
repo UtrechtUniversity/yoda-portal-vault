be7d51b6c6ad742eb2db8786a823c34630e49d82
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-07-26 12:48:02 +0200

    Revert "Move CSRF stuff to seperate file."
    This reverts commit 3def9d1fc27d6f7244408b1877c2abc1aa9ce09f.

--- public/static/js/post.js
+++
@@ -1,8 +0,0 @@
-$(document).ajaxSend(function(e, request, settings) {
-    // Append a CSRF token to all AJAX POST requests.
-    if (settings.type === 'POST' && settings.data.length) {
-         settings.data
-             += '&' + encodeURIComponent(YodaPortal.csrf.tokenName)
-              + '=' + encodeURIComponent(YodaPortal.csrf.tokenValue);
-    }
-});
--- public/static/js/research.js
+++ public/static/js/research.js
@@ -1,3 +1,12 @@
+$(document).ajaxSend(function(e, request, settings) {
+    // Append a CSRF token to all AJAX POST requests.
+    if (settings.type === 'POST' && settings.data.length) {
+         settings.data
+             += '&' + encodeURIComponent(YodaPortal.csrf.tokenName)
+              + '=' + encodeURIComponent(YodaPortal.csrf.tokenValue);
+    }
+});
+
 $( document ).ready(function() {
     if ($('#file-browser').length) {
         startBrowsing(browseStartDir, browsePageItems);
