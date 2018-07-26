6d2d8d407c756f4fac5b9637875440e30a95def1
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-07-26 12:44:15 +0200

    Merge files.

--- public/static/js/research.js
+++ public/static/js/research.js
@@ -1068,3 +1068,168 @@ function vaultAccess(action, folder)
         $('button.vault-access').prop("disabled", false);
     }, "json");
 }
+
+
+// search
+
+function search(value, type, itemsPerPage, displayStart, searchOrderDir, searchOrderColumn)
+{
+    if (typeof value != 'undefined' && value.length > 0 ) {
+        // Display start for first page load
+        if (typeof displayStart === 'undefined') {
+            displayStart = 0;
+        }
+
+        saveSearchRequest(value, type);
+
+        // Table columns definition
+        var disableSorting = {};
+        var columns = [];
+        if (type == 'filename') {
+            columns = ['Name', 'Location'];
+        } else if (type == 'metadata') {
+            columns = ['Location', 'Matches'];
+            disableSorting = { 'bSortable': false, 'aTargets': [ -1 ] };
+        } else {
+            columns = ['Location'];
+        }
+
+        // Destroy current Datatable
+        var datatable = $('#search').DataTable();
+        datatable.destroy();
+
+        var tableHeaders = '';
+        $.each(columns, function(i, val){
+            tableHeaders += "<th>" + val + "</th>";
+        });
+
+        // Create the columns
+        $('#search thead tr').html(tableHeaders);
+
+        // Remove table content
+        $('#search tbody').remove();
+
+	var encodedSearchString = encodeURIComponent(value);
+	/* limit the length of the encoded string to the worst case of 255*4*3=3060
+ 	*  maxLength of characters (255) * max bytes in UTF-8 encoded character (4) * URL encoding of byte (%HH) (3)
+ 	*/
+	if (encodedSearchString.length > 3060) {
+		setMessage('error', 'The search string is too long');
+		return true;
+	}
+
+        // Initialize new Datatable
+        var url = "search/data?filter=" + encodedSearchString + "&type=" + type;
+        $('#search').DataTable( {
+            "bFilter": false,
+            "bInfo": false,
+            "bLengthChange": false,
+            "language": {
+                "emptyTable": "Your search did not match any documents"
+            },
+            "ajax": {
+                "url": url,
+                "jsonp": false,
+                dataSrc: function (json) {
+                    jsonString = JSON.stringify(json);
+
+                    resp = JSON.parse(jsonString);
+
+                    //console.log(resp.draw);
+                    if (resp.status == 'Success' ) {
+                        return resp.data;
+                    }
+                    else {
+                        setMessage('error', resp.statusInfo);
+                        return true;
+                    }
+                }
+            },
+            "processing": true,
+            "serverSide": true,
+            "pageLength": searchPageItems,
+            "displayStart": displayStart,
+            "drawCallback": function(settings) {
+                $( ".browse-search" ).on( "click", function() {
+                    browse($(this).attr('data-path'));
+                });
+
+
+                $('.matches').tooltip();
+            },
+            "aoColumnDefs": [
+                disableSorting
+            ],
+            "ordering": false
+        });
+        //"order": [[ searchOrderColumn, searchOrderDir ]]  // save for future purposes - it intervenes with newly added "ordering": false
+
+        if (type == 'status') {
+            searchStatus = $(".search-status option:selected").text();
+            $('.search-string').text(searchStatus);
+        } else {
+            $('.search-string').html( htmlEncode(value).replace(/ /g, "&nbsp;") );
+        }
+    }
+
+    return true;
+}
+
+function closeSearchResults()
+{
+    $('.search-results').hide();
+    $('#search-filter').val('');
+    $('[name=status]').val('');
+    $.get("search/unset_session");
+}
+
+function showSearchResults()
+{
+    $('.search-results').show();
+}
+
+function searchSelectChanged(sel)
+{
+    $("#search_concept").html(sel.text());
+    $("#search_concept").attr('data-type', sel.attr('data-type'));
+
+    if (sel.attr('data-type') == 'status') {
+        $('.search-term').hide();
+        $('.search-status').removeClass('hide').show();
+	search($('.search-status').val(), 'status', $(".search-btn").attr('data-items-per-page'), 0, 'asc', 0);
+    } else {
+        $('.search-term').removeClass('hide').show();
+        $('.search-status').hide();
+	search($("#search-filter").val(), $("#search_concept").attr('data-type'), $(".search-btn").attr('data-items-per-page'), 0, 'asc', 0);
+    }
+}
+
+function saveSearchRequest(value, type)
+{
+    $.post( "search/set_session", { "value" : encodeURIComponent(value), "type" : type }, function(data) {
+        if(data.status == 'Success') {
+            if (type == 'revision' && view == 'revision') {
+                $('#search').hide();
+                $('.search-results').hide();
+                return false;
+            }
+
+            if (type == 'revision' && view == 'browse') {
+                $('#search').hide();
+                $('.search-results').hide();
+
+                window.location.href = "revision?filter=" + encodeURIComponent(value);
+                return false;
+            }
+
+            if (type != 'revision' && view == 'revision') {
+                window.location.href = "browse";
+                return false;
+            }
+
+            showSearchResults();
+        }
+    });
+}
+
+
