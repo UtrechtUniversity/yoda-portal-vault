e019d9c8f39356bd0eb156c9af04b259baedebdd
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-09-25 08:32:47 +0200

    Fix buttons when locked and no metadata exists.

--- app/index.js
+++ app/index.js
@@ -129,7 +129,7 @@ class YodaButtons extends React.Component {
     }
 
     renderButtons() {
-       if (isVaultPackage && isDatamanager) {
+        if (isVaultPackage && isDatamanager) {
             // Datamanager in Vault space.
             if (!updateButton && mode === "edit_in_vault") {
                 // Show 'Save' button.
@@ -138,14 +138,14 @@ class YodaButtons extends React.Component {
                 // Show 'Update' button.
                 return (<div>{this.renderUpdateButton()}</div>);
             }
-       } else if (writePermission) {
+        } else if (writePermission) {
             // Write permission in Research space.
-            if (!metadataExists && parentHasMetadata) {
-                // Show 'Save' and 'Clone from parent folder' buttons.
-                return (<div>{this.renderSaveButton()} {this.renderCloneButton()}</div>);
-            } else if (!metadataExists && locked) {
+            if (!metadataExists && locked) {
                 // Show no buttons.
                 return (<div></div>);
+            } else if (!metadataExists && parentHasMetadata) {
+                // Show 'Save' and 'Clone from parent folder' buttons.
+                return (<div>{this.renderSaveButton()} {this.renderCloneButton()}</div>);
             } else if (!locked && submitButton) {
                 // Show 'Save', 'Submit' and 'Delete all metadata' buttons.
                 return (<div> {this.renderSaveButton()} {this.renderSubmitButton()} {this.renderDeleteButton()}</div>);
@@ -497,7 +497,7 @@ function ArrayFieldTemplate(props) {
                 <div className="has-btn">
                     {element.children}
                     <div className={"btn-controls btn-group btn-count-" + btnCount} role="group">
-                        {canRemove && 
+                        {canRemove &&
                         <button type="button" className="clone-btn btn btn-default" onClick={item.onDropIndexClick(item.index)}>
                             <i class="fa fa-minus" aria-hidden="true"></i>
                         </button>}
