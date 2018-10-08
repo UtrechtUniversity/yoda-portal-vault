8bd648daf3eaa13ba2707e3ede1a49488cd08b12
Harm de Raaff <harm.apple@gmail.com>
2018-10-08 10:53:36 +0200

    fix YDA-2294/2295

--- models/Metadata_form_model.php
+++ models/Metadata_form_model.php
@@ -92,7 +92,7 @@ class Metadata_form_model extends CI_Model
         foreach ($jsonsElements['properties'] as $groupName => $formElements) {
             foreach ($formElements['properties'] as $mainElement => $element) {
 
-                if (isset($formData[$mainElement])) {
+                if (isset($formData[$mainElement]) && strlen($formData[$mainElement])) {
                     if (!isset($element['type'])
                         || $element['type']=='integer'
                         || $element['type']=='string' // string in situations like date on top level (embargo end date)
