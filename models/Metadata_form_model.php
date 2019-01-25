3bbb67c8fe77739ca006ed9502f264259f7c7056
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-01-25 15:05:36 +0100

    Add no namespace schema location to XML.

--- models/Metadata_form_model.php
+++ models/Metadata_form_model.php
@@ -90,10 +90,7 @@ class Metadata_form_model extends CI_Model
         $attributeXSI = $xml->createAttribute('xmlns:xsi');
         $attributeXSI->value = 'http://www.w3.org/2001/XMLSchema-instance';
 
-        $attributeXML = $xml->createAttribute('xmlns');
-        $attributeXML->value = $this->_getSchemaLocation($folder);  // to be determined dynamically via iRODS
-
-        $attributeXSD = $xml->createAttribute('xsi:schemaLocation');
+        $attributeXSD = $xml->createAttribute('xsi:noNamespaceSchemaLocation');
         $attributeXSD->value = $this->_getSchemaLocation($folder) . ' ' . $this->_getSchemaSpace($folder);  // to be determined dynamically via iRODS
 
         $xml_metadata->appendChild($attributeXSI);
