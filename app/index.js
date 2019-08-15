75bb9d8c8081407ef63c41310f84efa0552e8c40
jorisBarkema <jorisbarkema@gmail.com>
2019-08-15 14:39:09 +0200

    fix the bugs for real, add style correctly

--- app/index.js
+++ app/index.js
@@ -37,16 +37,28 @@ const numberWidget = (props) => {
     );
 };
 
+const customStyles = {
+	control: styles => ({...styles, borderRadius: '0px', minHeight: '15px', height: '33.5px'})
+};
+
 const enumWidget = (props) => {
 	
+	var enumArray = props["schema"]["enum"];
+	var enumNames = props["schema"]["enumNames"];
+
+	if (enumNames == null) enumNames = enumArray;
+
+	var i = enumArray.indexOf(props["value"]);
+
 	return (
 		<Select
 		className={"select-box"}
-		placeholder={props.value}
+		placeholder={enumNames[i]}
 		required={props.required}
-		isDisabled={props.enabled}
+		isDisabled={props.readonly}
 		onChange={(event) => props.onChange(event.value)}
-		options={props["options"]["enumOptions"]} />
+		options={props["options"]["enumOptions"]}
+		styles={customStyles} />
 	);
 };
 
