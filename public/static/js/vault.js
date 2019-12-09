49f7703ea330bbe5d4c9d8015a7425851163df0c
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-12-09 11:17:39 +0100

    Rename YodaPortal namespace to Yoda.

--- public/static/js/vault.js
+++ public/static/js/vault.js
@@ -2,8 +2,8 @@ $(document).ajaxSend(function(e, request, settings) {
     // Append a CSRF token to all AJAX POST requests.
     if (settings.type === 'POST' && settings.data.length) {
          settings.data
-             += '&' + encodeURIComponent(YodaPortal.csrf.tokenName)
-              + '=' + encodeURIComponent(YodaPortal.csrf.tokenValue);
+             += '&' + encodeURIComponent(Yoda.csrf.tokenName)
+              + '=' + encodeURIComponent(Yoda.csrf.tokenValue);
     }
 });
 
