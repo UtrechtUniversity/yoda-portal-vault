2694da2e221e5aafd78edad450a7ed3b29831d9b
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-11-26 10:42:43 +0100

    Replace panels with cards.

--- views/metadata/form.php
+++ views/metadata/form.php
@@ -1,15 +1,15 @@
 <div id="form-errors" class="row hide">
     <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-                <h3 class="panel-title pull-left">
+        <div class="card card-default">
+            <div class="card-header clearfix">
+                <h3 class="card-title pull-left">
                     Metadata form - <?php echo str_replace(' ', '&nbsp;', htmlentities(trim($path))); ?>
                 </h3>
                 <div class="input-group-sm has-feedback pull-right">
-                    <a class="btn btn-default" href="/vault/browse?dir=<?php echo rawurlencode($path); ?>">Close</a>
+                    <a class="btn btn-secondary" href="/vault/browse?dir=<?php echo rawurlencode($path); ?>">Close</a>
                 </div>
             </div>
-            <div class="panel-body">
+            <div class="card-body">
                 <p>
                     It is not possible to load this form as the yoda-metadata.json file is not
                     in accordance with the form definition.
@@ -25,16 +25,16 @@
 
 <div id="metadata-form" class="row hide">
     <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-                <h3 class="panel-title pull-left">
+        <div class="card card-default">
+            <div class="card-header clearfix">
+                <h5 class="card-title pull-left">
                     Metadata form - <?php echo str_replace(' ', '&nbsp;', htmlentities(trim($path))); ?>
-                </h3>
+                </h5>
                 <div class="input-group-sm has-feedback pull-right close-button">
-                    <a class="btn btn-default" href="/vault/browse?dir=<?php echo rawurlencode($path); ?>">Close</a>
+                    <a class="btn btn-secondary" href="/vault/browse?dir=<?php echo rawurlencode($path); ?>">Close</a>
                 </div>
             </div>
-            <div class="panel-body">
+            <div class="card-body">
                 <div id="no-metadata" class="hide">
                     <p>There is no metadata present for this folder.</p>
                 </div>
@@ -50,17 +50,17 @@
 </div>
 
 <script>
-// Show "loading" text if loading the form takes longer than expected.
-var formLoaded = false;
-setTimeout(function(){
-    if (!formLoaded) {
-        $('#metadata-form').fadeIn(200);
-        $('#metadata-form').removeClass('hide');
-    }
-}, 800);
+    // Show "loading" text if loading the form takes longer than expected.
+    var formLoaded = false;
+    setTimeout(function(){
+        if (!formLoaded) {
+            $('#metadata-form').fadeIn(200);
+            $('#metadata-form').removeClass('hide');
+        }
+    }, 800);
 </script>
 <script src="/vault/static/js/metadata/form.js" async></script>
 <script id="form-properties" type="text/plain"><?php
-// base64-encode to make sure no script tag can be embedded.
-echo base64_encode(json_encode($formProperties))
+    // base64-encode to make sure no script tag can be embedded.
+    echo base64_encode(json_encode($formProperties))
 ?></script>
